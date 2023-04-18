e0ed8795ef0df2a76bd86faba32e3873d4e98cea
AvidDabbler <walter.k.jenkins@gmail.com>
2023-04-17 21:42:38 -0500

    designing out about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { links, paths } from "../config";
 import Link from "next/link";
+import Image from "next/image";
+import Profile from "../assets/profile.png";
 
 export function About() {
   return (
     <div id="contact" className="my-auto h-full">
-      <div className="bg-blue topo flex h-screen w-full flex-col pt-14">
-        <div className="mx-auto grid w-1/2 gap-6">
-          <h2 className="font-md text-4xl italic text-orange-400">About Me</h2>
-          <p className="font-sans text-3xl font-medium leading-10 text-gray-200">
-            What's up! I'm Walter a data analyst turned software developer who
-            is passionate about telling stories with data. For 10 years I worked
-            for public transit agencies in the US and I am extremely passionate
-            about better government
+      <div className="flex w-full flex-col">
+        <div className="bg-blue topo grid w-full gap-8 py-32">
+          <div className=" mx-16 flex  justify-between gap-3">
+            <div className="grid">
+              <h2 className=" font-md text-5xl leading-10 text-orange-400">
+                About Me
+              </h2>
+              <div className="grid gap-12">
+                <p className=" text-4xl leading-normal text-gray-200">
+                  I'm Walter, a data analyst turned software developer who is
+                  passionate about telling stories with data.
+                </p>
+              </div>
+            </div>
+
+            <Image
+              src={Profile}
+              className="rounded-full ring-4 drop-shadow-lg"
+              alt="profile photo"
+              width={"300"}
+            ></Image>
+          </div>
+
+          <p className="mx-16 text-4xl leading-normal text-gray-200 lg:mx-auto lg:w-2/3">
+            I like to build tools that turn complex work into simple tools and
+            data visualizations that make sense to non-techies.
           </p>
-          <Link href={paths.home} className="text-white">
-            Learn More
-          </Link>
         </div>
-        <div className=" w my-20 flex flex-col justify-center rounded-lg text-center">
+        <div className="bg-blue-500/30 bg-opacity-95 w py-20 flex flex-col justify-center rounded-lg text-center">
           <h2 className="text-center text-xl text-white">My Things</h2>
           <div className="m-6 flex flex-col text-center text-white">
             <Link className="link m-6" href={paths.projects}>
