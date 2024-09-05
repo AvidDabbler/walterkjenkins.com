b6527b3b8fd396ff5dbdee9624982be815e6069a
Walter Jenkins <walter@transit.chat>
2024-09-05 10:00:33 -0500

    trusted by

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -15,11 +15,34 @@ export const AboutWalter = () => {
           width={200}
           height={200}
         ></Image>
-        <div>
-          <h1 className=" text-center text-4xl font-bold">Walter Jenkins</h1>
-          <h2 className="text-center text-2xl font-medium text-orange-500">
+        <div className="flex flex-col gap-6 pb-8">
+          <h1 className="pacifico text-center text-6xl">Walter Jenkins</h1>
+          <h2 className="text-center text-3xl font-medium text-orange-500">
             Developer and Analyst helping companies tell stories with there data
           </h2>
+          <div className="flex flex-wrap justify-center gap-3">
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Startups
+            </p>
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Data Analysis
+            </p>
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Mobile app development
+            </p>
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Web Mapping
+            </p>
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Technical Project Management
+            </p>
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Product Design
+            </p>
+            <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
+              Web Development
+            </p>
+          </div>
           <div className="my-2 flex justify-center">
             <Link
               className="mx-auto rounded-full bg-green-400 px-4 py-2 text-2xl transition-all hover:bg-gray-600"
@@ -29,38 +52,58 @@ export const AboutWalter = () => {
             </Link>
           </div>
         </div>
-        <div className="flex flex-wrap justify-center gap-3">
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Startups
-          </p>
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Data Analysis
-          </p>
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Mobile app development
-          </p>
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Web Mapping
-          </p>
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Technical Project Management
-          </p>
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Product Design
-          </p>
-          <p className="mx-1 flex flex-wrap justify-center rounded-2xl bg-orange-400 px-2 text-xl font-medium capitalize text-white">
-            Web Development
-          </p>
-        </div>
-        <p className="text-3xl font-bold">Hey There! 👋</p>
+        <p className="text-center text-4xl font-bold">Hey There! 👋</p>
         <p>
-          I'm <span className="highlight">Walter Jenkins</span>, a full-stack
-          developer with a passion for creating impactful digital solutions for
-          the public sector. With over 13 years of experience in GIS and 5 years
-          in modern web development, I specialize in delivering custom website
-          and mobile applications that make a real difference in startups, urban
-          planning and public transit.
+          I'm <span className="highlight font-medium">Walter Jenkins</span>, a
+          full-stack developer with focused on telling stories with data and
+          solutions for the public sector. My clients often have a story they
+          want to tell and I help them write it. Whether it is using maps of
+          communities or stories told with charts and tables, I focus on making
+          sure that the reader walks away smarter by using as few words as
+          possible.
         </p>
+      </div>
+      <h2 className="mx-auto text-4xl font-bold text-white">Trusted By</h2>
+      <div className="mx-auto flex flex-wrap justify-center gap-6">
+        <div className=" flex h-[250px] w-[250px] items-center rounded-full bg-white p-6">
+          <Image
+            src={"/images/clt1.jpg"}
+            alt="headshot of walter"
+            className="rounded-full bg-white"
+            width={250}
+            height={250}
+          ></Image>
+        </div>
+        <div className="flex h-[250px] w-[250px] items-center rounded-full bg-white p-6">
+          <Image
+            src={"/images/metrostl.png"}
+            alt="headshot of walter"
+            className="mx-auto bg-white"
+            width={200}
+            height={200}
+          ></Image>
+        </div>
+        <div className="flex h-[250px] w-[250px] items-center rounded-full bg-white p-6">
+          <Image
+            src={"/images/un.png"}
+            alt="headshot of walter"
+            className="mx-auto bg-white"
+            width={200}
+            height={100}
+          ></Image>
+        </div>
+        <div className="flex h-[250px] w-[250px] items-center rounded-full bg-white p-6">
+          <Image
+            src={"/images/world-central-kitchen.png"}
+            alt="headshot of walter"
+            className="mx-auto bg-white"
+            width={200}
+            height={100}
+          ></Image>
+        </div>
+      </div>
+
+      <div className="mx-auto flex w-full max-w-[600px] flex-col gap-6 px-6  text-white md:w-1/2">
         <Image
           src={"/blog-images/3d-land-value/demo.gif"}
           alt="Land value demo"
@@ -69,27 +112,35 @@ export const AboutWalter = () => {
         ></Image>
         <h2 className="text-xl font-bold">What I Offer</h2>
         <p>
-          My goal is to make technology work for you, even if you're not
-          tech-savvy. Whether you're looking to develop a website, a mobile app,
-          or need in-depth data analysis to improve your workflows, I'm here to
-          help. Here's what I can do for you:
+          While I can bring your story to life with data, I am especially fond
+          of working on websites that help the public sector make an impact. As
+          a former public servant I know how hard it is to get things done and I
+          help cut through and do things that the public sector might take a
+          long time to.
         </p>
-        <ul className="list-disc">
+        <p>
+          In the past I have developed websites for causes, organizations and
+          buisinesses, developed apps for organizations like the World Central
+          Kitchen and built analysis workflows for the United Nations.
+        </p>
+        <ul className="grid list-disc gap-6 px-12 ">
           <li>
-            <span className="highlight">Website and Mobile Development:</span> I
-            use React and Node.js to build responsive, user-friendly websites
+            <span className="highlight font-medium">
+              Website and Mobile Development:
+            </span>{" "}
+            I use React and Node.js to build responsive, user-friendly websites
             and mobile applications tailored to your needs.
           </li>
           <li>
-            <span className="highlight">Data Analysis:</span> Leveraging web
-            technologies, I can provide insightful analysis that helps
-            streamline your operations, especially in areas like public transit
-            and urban planning.
+            <span className="highlight font-medium">Data Analysis:</span>{" "}
+            Leveraging web technologies, I can provide insightful analysis that
+            helps streamline your operations, especially in areas like public
+            transit and urban planning.
           </li>
           <li>
-            <span className="highlight">Custom Solutions:</span> Every project
-            is unique. I work closely with you to understand your goals and
-            deliver solutions that are both effective and easy to use.
+            <span className="highlight font-medium">Custom Solutions:</span>{" "}
+            Every project is unique. I work closely with you to understand your
+            goals and deliver solutions that are both effective and easy to use.
           </li>
         </ul>
 
